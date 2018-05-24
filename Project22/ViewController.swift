d394edf4d2d07fa60471beef05ad99966844b3a7
Sabrina <sfletc01@gmail.com>
2018-05-24 16:02:15 -0400

    Tells you how far you are from the beacon transmitting a signal

--- Project22/ViewController.swift
+++ Project22/ViewController.swift
@@ -6,13 +6,81 @@
 //  Copyright © 2018 Sabrina Fletcher. All rights reserved.
 //
 
+
+//This app requires you to download locateBeacon app, and adjust the Beacon info for Apple AirLocate 5A4BCFCE so that its major and minor match that within the startScanning() method, then transmit that beacon and run this app, it will let you know how near or far that beacon is on the screen
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    var locationManager: CLLocationManager!
+
+    @IBOutlet weak var distanceReading: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        
+        view.backgroundColor = UIColor.gray
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways{
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self){
+                if CLLocationManager.isRangingAvailable() {
+                    startScanning()
+                }
+            }
+        }
+    }
+    
+    func startScanning(){
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.startRangingBeacons(in: beaconRegion)
+    }
+    
+    func update(distance: CLProximity){
+        //changes the screen color and text color depending on the proximity of the beacon
+        UIView.animate(withDuration: 0.8){
+            [unowned self] in
+            switch distance {
+            case .unknown:
+                self.view.backgroundColor = UIColor.gray
+                self.distanceReading.text = "UNKNOWN"
+                
+            case .far:
+                self.view.backgroundColor = UIColor.blue
+                self.distanceReading.text = "FAR"
+                
+            case .near:
+                self.view.backgroundColor = UIColor.orange
+                self.distanceReading.text = "NEAR"
+                
+            case .immediate:
+                self.view.backgroundColor = UIColor.red
+                self.distanceReading.text = "RIGHT HERE"
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        //catch the ranging method from CLLocationManager
+        if beacons.count > 0 {
+            //if beacons are received, we'll pull out the first one and use its proximity property to call our update method and redraw user interface
+            let beacon = beacons[0]
+            update(distance: beacon.proximity)
+        } else{
+            //no beacons found
+            update(distance: .unknown)
+        }
     }
 
     override func didReceiveMemoryWarning() {
